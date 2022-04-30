dcbeb72f49a18cbaa8f78e5fea29391b4181661a
Reeul <chotnt741@gmail.com>
2022-05-01 02:26:00 +0900

    카드 리스트 모바일 최적화

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -4,7 +4,7 @@ import CardList from "../components/CardList.vue";
 
 <template>
   <main class="flex justify-end w-screen">
-    <main class="h-screen lg:w-2/3">
+    <main class="h-screen w-screen lg:w-2/3">
       <CardList />
     </main>
   </main>
